df5520cc3be1eb3b5d66fc8c5ae4cfa7ce5266f7
Max Huber <huber.maxi@gmail.com>
2025-12-07 18:41:15 +0100

    tag 7

--- tag7/src/main.rs
+++ tag7/src/main.rs
@@ -1,3 +1,46 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("./input")
+        .expect("could not read")
+        .replace("S", "|");
+    let mut lines: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
+    let mut count: Vec<Vec<u128>> = content
+        .lines()
+        .map(|l| {
+            l.chars()
+                .map(|c| match c {
+                    '|' => 1,
+                    _ => 0,
+                })
+                .collect()
+        })
+        .collect();
+
+    let mut split: u128 = 0;
+    for row in 1..lines.len() {
+        for col in 0..lines[0].len() {
+            match lines[row][col] {
+                '^' => {
+                    if lines[row - 1][col] == '|' {
+                        lines[row][col - 1] = '|';
+                        lines[row][col + 1] = '|';
+                        count[row][col - 1] += count[row - 1][col];
+                        count[row][col + 1] += count[row - 1][col];
+                    }
+                }
+                '.' => {
+                    if lines[row - 1][col] == '|' {
+                        lines[row][col] = '|';
+                        count[row][col] += count[row - 1][col];
+                    }
+                }
+                _ => count[row][col] += count[row - 1][col],
+            }
+        }
+    }
+    for n in count[count.len() - 1].iter() {
+        split += n;
+    }
+    println!("{}", split);
 }
