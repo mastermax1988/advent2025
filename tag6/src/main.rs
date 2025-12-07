e567614bb9e506f274f9c26e26f1be4a9d240fd0
Max Huber <huber.maxi@gmail.com>
2025-12-07 16:41:18 +0100

    tag 6 gelöst

--- tag6/src/main.rs
+++ tag6/src/main.rs
@@ -1,7 +1,44 @@
-use std::{clone, fs};
+use core::num;
+use std::fs;
 
 fn main() {
     let content = fs::read_to_string("./input").expect("could not read");
+    let lines: Vec<Vec<char>> = content.lines().map(|l| l.chars().collect()).collect();
+    let mut numbers: Vec<u64> = Vec::new();
+    let mut sum: u64 = 0;
+    for i in 0..lines[0].len() {
+        let col = lines[0].len() - 1 - i;
+        let mut numstr = "".to_string();
+        for row in 0..lines.len() {
+            if lines[row][col] == '+' || lines[row][col] == '*' {
+                numbers.push(numstr.parse().unwrap());
+                numstr.clear();
+                let mut e: u64 = match lines[row][col] {
+                    '+' => 0,
+                    _ => 1,
+                };
+                for n in &numbers {
+                    match lines[row][col] {
+                        '+' => e += n,
+                        _ => e *= n,
+                    }
+                }
+                sum += e;
+                numbers.clear();
+                continue;
+            }
+            if lines[row][col] != ' ' {
+                numstr.push(lines[row][col]);
+            }
+        }
+        match numstr.parse() {
+            Ok(m) => numbers.push(m),
+            _ => (),
+        }
+    }
+    println!("{}", sum);
+    return;
+
     let mut elements: Vec<Vec<u64>> = Vec::new();
     let mut op: Vec<char> = Vec::new();
     for line in content.lines() {
